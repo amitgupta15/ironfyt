12a66053652f9e05640565fb5bfb6441ae228195
Amit Gupta <amit.gupta@arrowts.com>
2020-07-02 09:54:42 -0700

    stable

--- public/js/app-new.js
+++ public/js/app-new.js
@@ -6,12 +6,12 @@
   /**
    * Event Listeners
    */
-  let eventHandlers = {
+  let eventTarget = {
     'new-task-btn': {
       fn: showModal,
       props: {
         elementId: '#select-task-modal',
-        modalType: 'slideUp',
+        slideUpModal: true,
       },
     },
     'close-select-task-modal': {
@@ -24,7 +24,6 @@
       fn: showModal,
       props: {
         elementId: '#new-workout-form-modal',
-        modalType: 'slideLeft',
       },
     },
     'close-new-workout-modal-btn': {
@@ -33,11 +32,23 @@
         elementId: '#new-workout-form-modal',
       },
     },
+    'new-log-modal-btn': {
+      fn: showModal,
+      props: {
+        elementId: '#log-workout-modal',
+      },
+    },
+    'close-log-workout-modal-btn': {
+      fn: hideModal,
+      props: {
+        elementId: '#log-workout-modal',
+      },
+    },
   };
   document.addEventListener('click', function (event) {
     event.preventDefault();
-    if (eventHandlers[event.target.id] !== undefined) {
-      let handler = eventHandlers[event.target.id];
+    if (eventTarget[event.target.id] !== undefined) {
+      let handler = eventTarget[event.target.id];
       handler.fn(handler.props);
     }
   });
@@ -46,10 +57,10 @@
    * Handlers
    */
   function showModal(props) {
-    let { elementId, modalType } = props;
+    let { elementId, slideUpModal } = props;
     let dialog = document.querySelector(elementId);
     if (visibleModals.indexOf(elementId) < 0) {
-      dialog.style.transform = modalType === 'slideUp' ? 'translateY(-100vh)' : 'translateX(-100vw)';
+      dialog.style.transform = slideUpModal !== undefined && slideUpModal === true ? 'translateY(-100vh)' : 'translateX(-100vw)';
       visibleModals.push(props);
     }
   }
@@ -57,7 +68,7 @@
     let dialog = document.querySelector(elementId);
     let obj = visibleModals.find((item) => item.elementId === elementId);
     if (visibleModals.indexOf(obj) > -1) {
-      dialog.style.transform = obj.modalType === 'slideUp' ? 'translateY(100vh)' : 'translateX(100vw)';
+      dialog.style.transform = obj.slideUpModal !== undefined && obj.slideUpModal === true ? 'translateY(100vh)' : 'translateX(100vw)';
       let index = visibleModals.indexOf(obj);
       visibleModals.splice(index, 1);
     }
