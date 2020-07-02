08ccb1f0fc430046e8665acce7bae8a68b43e7b0
Amit Gupta <amit.gupta@arrowts.com>
2020-07-02 16:39:05 -0700

    working modals

--- public/js/app-new.js
+++ public/js/app-new.js
@@ -6,12 +6,13 @@
   /**
    * Event Listeners
    */
+
   let eventTarget = {
     'new-task-btn': {
       fn: showModal,
       props: {
         elementId: '#select-task-modal',
-        slideUpModal: true,
+        isSlideUpModal: true,
       },
     },
     'close-select-task-modal': {
@@ -44,6 +45,24 @@
         elementId: '#log-workout-modal',
       },
     },
+    'log-activity-modal-btn': {
+      fn: showModal,
+      props: {
+        elementId: '#log-activity-modal',
+      },
+    },
+    'close-log-activity-modal-btn': {
+      fn: hideModal,
+      props: {
+        elementId: '#log-activity-modal',
+      },
+    },
+    'save-log-activity-modal-btn': {
+      fn: saveAndCloseModal,
+      props: {
+        elementId: '#log-activity-modal',
+      },
+    },
   };
   document.addEventListener('click', function (event) {
     event.preventDefault();
@@ -57,10 +76,10 @@
    * Handlers
    */
   function showModal(props) {
-    let { elementId, slideUpModal } = props;
+    let { elementId, isSlideUpModal } = props;
     let dialog = document.querySelector(elementId);
     if (visibleModals.indexOf(elementId) < 0) {
-      dialog.style.transform = slideUpModal !== undefined && slideUpModal === true ? 'translateY(-100%)' : 'translateX(-100%)';
+      dialog.style.transform = isSlideUpModal !== undefined && isSlideUpModal === true ? 'translateY(-100%)' : 'translateX(-100%)';
       visibleModals.push(props);
     }
   }
@@ -68,9 +87,31 @@
     let dialog = document.querySelector(elementId);
     let obj = visibleModals.find((item) => item.elementId === elementId);
     if (visibleModals.indexOf(obj) > -1) {
-      dialog.style.transform = obj.slideUpModal !== undefined && obj.slideUpModal === true ? 'translateY(100%)' : 'translateX(100%)';
+      dialog.style.transform = obj.isSlideUpModal !== undefined && obj.isSlideUpModal === true ? 'translateY(100%)' : 'translateX(100%)';
       let index = visibleModals.indexOf(obj);
       visibleModals.splice(index, 1);
     }
+  }
+
+  function saveAndCloseModal(props) {
+    visibleModals.forEach((modal) => {
+      // Get the modal dialog
+      let dialog = document.querySelector(modal.elementId);
+      dialog.style.transitionDuration = '0s';
+      if (modal.isSlideUpModal) {
+        dialog.style.transform = 'translateY(100%)';
+      } else {
+        dialog.style.transform = 'translateX(100%)';
+      }
+    });
+
+    // Resetting all styles after a short delay to avoid interference with above transitions
+    window.setTimeout(function () {
+      visibleModals.forEach((modal) => {
+        let dialog = document.querySelector(modal.elementId);
+        dialog.style = '';
+      });
+      visibleModals = [];
+    }, 100);
   }
 })();
