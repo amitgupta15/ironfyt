5035e67b765cb37e0abc5ffb16bda7b14a3fa1e1
Amit Gupta <amitgupta15@gmail.com>
2020-08-12 09:31:35 -0700

    refactor

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -24,13 +24,13 @@
    * @param {*} month //month is 1-indexed
    * @param {*} date //date field is optional and has no effect on the final result. It is used for recursion
    */
-  ironfytCal.daysInMonth = function (year, month, date = 1) {
-    let aDate = new Date(year, month - 1, date);
-    if (aDate.getDate() !== date) {
-      return new Date(year, month - 1, date - 1).getDate();
-    }
-    return ironfytCal.daysInMonth(year, month, date + 1);
-  };
+  // ironfytCal.daysInMonth = function (year, month, date = 1) {
+  //   let aDate = new Date(year, month - 1, date);
+  //   if (aDate.getDate() !== date) {
+  //     return new Date(year, month - 1, date - 1).getDate();
+  //   }
+  //   return ironfytCal.daysInMonth(year, month, date + 1);
+  // };
 
   let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   let daysofWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -42,10 +42,47 @@
   let month = currentMonth;
   let year = currentYear;
 
+  let firstDayOfCalendarMonth = new Date(year, month).getDay();
+  let totalDaysInCalendarMonth = new Date(year, month + 1, 0).getDate();
+  let totalDaysInPrevCalendarMonth = new Date(year, month, 0).getDate();
+  let daysOfCalendarMonth = [];
+
+  for (var i = 1; i <= firstDayOfCalendarMonth; i++) {
+    daysOfCalendarMonth.push({ date: totalDaysInPrevCalendarMonth - firstDayOfCalendarMonth + i, month: 'prev' });
+  }
+  for (var i = 1; i <= totalDaysInCalendarMonth; i++) {
+    daysOfCalendarMonth.push({ date: i, month: 'current' });
+  }
+  if (daysOfCalendarMonth.length < 42) {
+    let count = 42 - daysOfCalendarMonth.length;
+    for (var i = 1; i <= count; i++) {
+      daysOfCalendarMonth.push({ date: i, month: 'next' });
+    }
+  }
+
+  ironfytCal.dateGridTemplate = function (props) {
+    return props.daysOfCalendarMonth
+      .map((day) => {
+        if (day.month === 'prev') return `<div id="dt${props.year}${props.monthDigit}${day.date}" class="calendar-item date-last-month">${day.date}</div>`;
+        if (day.month === 'current') {
+          return `<div id="dt${props.year}${props.monthDigit + 1}${day.date}" class="calendar-item date  for-time-border">${day.date}
+                  <div class="modality-indicator-container">
+                    <div class="modality-indicator m"></div>
+                    <div class="modality-indicator w"></div>
+                    <div class="modality-indicator g"></div>
+                  </div>
+                  </div>`;
+        }
+        if (day.month === 'next') return `<div id="dt${props.year}${props.monthDigit + 2}${day.date}" class="calendar-item date-next-month">${day.date}</div>`;
+      })
+      .join('');
+  };
   ironfytCal.calendarComponent = new Component('[data-app=calendar]', {
     data: {
+      monthDigit: '',
       month: '',
       year: '',
+      daysOfCalendarMonth: [],
     },
     template: function (props) {
       return `
@@ -55,180 +92,22 @@
            <h2>${props.month} ${props.year}</h2>
            <button id="next-month-btn">Next</button>
         </div>
+        <div class="calendar">
+          <div class="calendar-item day">S</div>
+          <div class="calendar-item day">M</div>
+          <div class="calendar-item day">T</div>
+          <div class="calendar-item day">W</div>
+          <div class="calendar-item day">TH</div>
+          <div class="calendar-item day">F</div>
+          <div class="calendar-item day">SA</div>
+          ${ironfytCal.dateGridTemplate(props)}
+        </div>
       </div>
       `;
     },
   });
 
-  ironfytCal.calendarComponent.setData({ month: months[month], year: year });
-  // let calendarComponent = new Component('[data-app=calendar]', {
-  //   data: {
-  //     month: '',
-  //     year: '',
-  //   },
-  //   template: function (props) {
-  //     return `
-  // <div class="calendar-container">
-  //       <div class="calendar-month-bar">
-  //         <button id="prev-month-btn">Previous</button>
-  //         <h2>${months[month]} ${year}</h2>
-  //         <button id="next-month-btn">Next</button>
-  //       </div>
-  //       <div class="calendar">
-  //         <div class="calendar-item day">S</div>
-  //         <div class="calendar-item day">M</div>
-  //         <div class="calendar-item day">T</div>
-  //         <div class="calendar-item day">W</div>
-  //         <div class="calendar-item day">TH</div>
-  //         <div class="calendar-item day">F</div>
-  //         <div class="calendar-item day">SA</div>
-  //         <div id="dt200628" class="calendar-item date-last-month">28</div>
-  //         <div id="dt200629" class="calendar-item date-last-month for-time-border">
-  //           29
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator m"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200630" class="calendar-item date-last-month for-load-border">
-  //           30
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator w"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200701" class="calendar-item date for-time-border">
-  //           1
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200702" class="calendar-item date">2</div>
-  //         <div id="dt200703" class="calendar-item date for-time-border">
-  //           <span class="selected-date">3</span>
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator m"></div>
-  //             <div class="modality-indicator w"></div>
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200704" class="calendar-item date amrap-border">
-  //           4
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator w"></div>
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200705" class="calendar-item date amrap-border">
-  //           5
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200706" class="calendar-item date for-load-border">
-  //           6
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator w"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200707" class="calendar-item date for-time-border">
-  //           7
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator m"></div>
-  //             <div class="modality-indicator w"></div>
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200708" class="calendar-item date amrap-border">
-  //           8
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200709" class="calendar-item date">9</div>
-  //         <div id="dt200710" class="calendar-item date">10</div>
-  //         <div id="dt200711" class="calendar-item date for-load-border">
-  //           11
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator w"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200712" class="calendar-item date amrap-border">
-  //           12
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200713" class="calendar-item date for-time-border">
-  //           13
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator m"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200714" class="calendar-item date for-time-border">
-  //           14
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator w"></div>
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200715" class="calendar-item date for-load-border">
-  //           15
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator w"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200716" class="calendar-item date">16</div>
-  //         <div id="dt200717" class="calendar-item date for-time-border">
-  //           17
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator m"></div>
-  //             <div class="modality-indicator w"></div>
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200718" class="calendar-item date for-time-border">
-  //           18
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator w"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200719" class="calendar-item date">19</div>
-  //         <div id="dt200720" class="calendar-item date for-time-border">
-  //           20
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator m"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200721" class="calendar-item date for-time-border">
-  //           21
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200722" class="calendar-item date current-date for-time-border">
-  //           22
-  //           <div class="modality-indicator-container">
-  //             <div class="modality-indicator m"></div>
-  //             <div class="modality-indicator w"></div>
-  //             <div class="modality-indicator g"></div>
-  //           </div>
-  //         </div>
-  //         <div id="dt200723" class="calendar-item date">23</div>
-  //         <div id="dt200724" class="calendar-item date">24</div>
-  //         <div id="dt200725" class="calendar-item date">25</div>
-  //         <div id="dt200726" class="calendar-item date">26</div>
-  //         <div id="dt200727" class="calendar-item date">27</div>
-  //         <div id="dt200728" class="calendar-item date">28</div>
-  //         <div id="dt200729" class="calendar-item date">29</div>
-  //         <div id="dt200730" class="calendar-item date">30</div>
-  //         <div id="dt200731" class="calendar-item date">31</div>
-  //         <div id="dt200801" class="calendar-item date-next-month">1</div>
-  //       </div>
-  //     </div>
-  // `;
-  //   },
-  // });
-  // let calendarContainer = document.getElementById('calendar-container');
-  // calendarContainer.innerHTML = ;
+  ironfytCal.calendarComponent.setData({ monthDigit: month, month: months[month], year: year, daysOfCalendarMonth });
 
   function showPrevMonth() {
     console.log('show prev month');
