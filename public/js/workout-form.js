07fbc2f307d3e45aba9220a06cbf350562e36fdb
Amit Gupta <amitgupta15@gmail.com>
2021-12-01 13:50:01 -0800

    storing prelim workout data in localstorage

--- public/js/workout-form.js
+++ public/js/workout-form.js
@@ -1,8 +1,12 @@
 (function () {
   'use strict';
 
+  let newPlaceholderWorkout = {
+    name: null,
+    description: null,
+  };
   let workoutFormTemplate = function (props) {
-    let workout = props && props.workout ? props.workout : {};
+    let workout = props && props.workout ? props.workout : newPlaceholderWorkout;
     return `
     <div class="container">
       <div class="text-align-center bold-text">Name & Description</div>
@@ -10,11 +14,11 @@
       <form id="workout-form">
         <div class="margin-top-20px">
           <label for="workout-name" class="form-label-classic">Name</label>
-          <input type="text" class="form-input-classic" name="workout-name" maxlength="30" id="workout-name" placeholder="" value="" required autofocus>
+          <input type="text" class="form-input-classic" name="workout-name" maxlength="30" id="workout-name" placeholder="" value="${workout.name ? workout.name : ''}" required autofocus>
         </div>
         <div class="margin-top-10px">
           <label for="workout-description" class="form-label-classic">Description</label>
-          <textarea class="form-input-classic" name="workout-description" id="workout-description" placeholder="" required></textarea>
+          <textarea class="form-input-classic" name="workout-description" id="workout-description" placeholder="" required>${workout.description ? workout.description : ''}</textarea>
         </div>
         <div class="submit-btn-bar margin-top-5px">
           <button type="button" id="new-workout-next-step-btn" class="submit-btn">Next</button>
@@ -29,7 +33,7 @@
       user: {},
       error: '',
       validationError: {},
-      workout: $ironfyt.newWorkout,
+      workout: newPlaceholderWorkout,
       pageTitle: 'New Workout',
     },
     template: function (props) {
@@ -38,6 +42,9 @@
   }));
 
   let handleNextStepButton = function () {
+    let name = document.getElementById('workout-name').value;
+    let description = document.getElementById('workout-description').value;
+    localStorage.setItem('newworkout', JSON.stringify({ name, description }));
     $ironfyt.navigateToUrl(`workout-form-review.html`);
   };
 
@@ -45,7 +52,13 @@
     $ironfyt.authenticateUser(function (error, auth) {
       if (!error) {
         let user = auth.user;
-        component.setState({ user });
+        let workout = localStorage.getItem('newworkout');
+        if (workout === null) {
+          workout = newPlaceholderWorkout;
+        } else {
+          workout = JSON.parse(workout);
+        }
+        component.setState({ user, workout });
       } else {
         component.setState({ error });
       }
